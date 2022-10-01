aa5d1c2dbf24d23bf2e4e742a26b4db11a2b3cd7
mpiccato <mpiccato@me.com>
2022-10-01 10:36:36 -0300

    primer commit

---
+++ src/app/firebase.js
@@ -0,0 +1,17 @@
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+   apiKey: "AIzaSyBvEF2v0FPPv8Y_26YZLYJT2ar0tuvnIm0",
+   authDomain: "fir-auth-5f63e.firebaseapp.com",
+   projectId: "fir-auth-5f63e",
+   storageBucket: "fir-auth-5f63e.appspot.com",
+   messagingSenderId: "778730612169",
+   appId: "1:778730612169:web:ba963de41e33ae61fb7418"
+ };
+
+ // Initialize Firebase
+ export const app = initializeApp(firebaseConfig);
